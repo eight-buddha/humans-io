629cfc92fea1d84b5ef5063ed9c421029b2e171a
Sasha Rudenko <sasha.rudenko@gmail.com>
2022-08-05 12:43:29 +0300

    added camera support

--- store/actions/human.js
+++ store/actions/human.js
@@ -1,49 +1,32 @@
-//import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system";
 
 
 export const ADD_HUMAN = "ADD_HUMAN";
 
 export const addHuman = (name, image) => {
     return async dispatch => {
-        dispatch({
-            type: ADD_HUMAN,
-            humanData: {
-                id: (new Date()).getTime(),
-                name,
-                image: "https://i1.sndcdn.com/artworks-000650294095-pjdx6p-t240x240.jpg"
-            }
-        })
+
+        const fileName = image.split('/').pop();
+        const newPath = FileSystem.documentDirectory + fileName;
+
+        try {
+            await FileSystem.moveAsync({
+                from: image,
+                to: newPath
+            });
+
+            dispatch({
+                type: ADD_HUMAN,
+                humanData: {
+                    id: (new Date()).getTime(),
+                    name,
+                    image: newPath
+                }
+            });
+
+        } catch (e) {
+            console.log('Move error:', e);
+            throw e;
+        }
     }
-    // return {
-    //     type: ADD_HUMAN,
-    //     humanData: {
-    //         id: (new Date()).getTime(),
-    //         name,
-    //         image: "https://i1.sndcdn.com/artworks-000650294095-pjdx6p-t240x240.jpg"
-    //     }
-    // };
-    // return async dispatch => {
-    //     const fileName = image.split('/').pop();
-    //     const newPath = FileSystem.documentDirectory;// + fileName;
-
-    //     try {
-
-    //         await FileSystem.moveAsync({
-    //             from: image,
-    //             to: newPath + fileName
-    //         });
-
-    //         return {
-    //             type: ADD_HUMAN,
-    //             humanData: {
-    //                 id: (new Date()).getTime(),
-    //                 name,
-    //                 image: newPath
-    //             }
-    //         };
-    //     } catch (e) {
-    //         console.log(e);
-    //         throw e;
-    //     }
-    // }
 }
